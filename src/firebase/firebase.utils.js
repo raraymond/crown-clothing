4332682f8573dce1dc8aa40038c1db35b875a942
Ryan Raymond <ryan@iMac.local>
2020-04-20 12:29:05 -0400

    finishing signin handlesumbit method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -20,8 +20,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) =>{
 
   const snapShot = await userRef.get();
 
-  console.log('SnapShot:',snapShot)
-
   if(!snapShot.exists){
     const {displayName, email} = userAuth;
     const createdAt = new Date();
