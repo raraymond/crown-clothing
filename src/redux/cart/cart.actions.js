eb101c189594abe0ad3a41fa2fe037481229324a
Ryan Raymond <ryan@iMac.local>
2020-04-20 22:27:35 -0400

    finished adding items and removing items from cart page basic routing and functionality complete from a front view perspective still more to do

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,4 +1,5 @@
 import CartActionTypes from './cart.types'
+import cartActionTypes from './cart.types'
 
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN
@@ -9,3 +10,12 @@ export const addItem = item =>({
   payload: item
 })
 
+export const removeItem = item =>({
+  type: cartActionTypes.REMOVE_ITEM,
+  payload: item
+})
+
+export const clearItemFromCart = item => ({
+  type: CartActionTypes.CLEAR_ITEM_FROM_CART,
+  payload: item
+})
