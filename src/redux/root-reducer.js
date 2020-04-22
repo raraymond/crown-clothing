584d3b9d8d91d2c13c812185b4604bfb5e5a4a33
Ryan Raymond <ryan@iMac.local>
2020-04-21 22:19:30 -0400

    added directory reducer to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,16 +4,20 @@ import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
 
 const persistConfig = {
 	key       : 'root',
 	storage,
-	whitelist : [ 'cart' ]
+	whitelist : [
+		'cart',
+	],
 };
 
 const rootReducer = combineReducers({
-	user : userReducer,
-	cart : cartReducer
+	user      : userReducer,
+	cart      : cartReducer,
+	directory : directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
