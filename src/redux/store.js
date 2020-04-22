c1a701aded7ea7a9c93883503a62f33020e5c9f9
Ryan Raymond <ryan@iMac.local>
2020-04-22 13:18:48 -0400

    updated store to remove logger in production

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ import rootReducer from './root-reducer';
 
 const middlewares = [];
 
-if (ProcessingInstruction.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
 	middlewares.push(logger);
 }
 
