e3f237628b59867b4445db90a0b059ea32762005
Ryan Raymond <ryan@iMac.local>
2020-04-21 22:36:17 -0400

    added shop reducer and moved shop data to the new shp data reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,33 +1,20 @@
 import React from 'react';
-
-import SHOP_DATA from './shop.data.js';
-
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends React.Component {
-	constructor (props) {
-		super(props);
+import { selectCollections } from '../../redux/shop/shop.selector';
 
-		this.state = {
-			collections : SHOP_DATA
-		};
-	}
+const ShopPage = ({ collections }) => (
+	<div className='shop-page'>
+		{collections.map(({ id, ...otherCollectionProps }) => (
+			<CollectionPreview key={id} {...otherCollectionProps} />
+		))}
+	</div>
+);
 
-	render () {
-		const { collections } = this.state;
-		return (
-			<div className="shop-page">
-				{collections.map(
-					({ id, ...otherCollectionProps }) => (
-						<CollectionPreview
-							key={id}
-							{...otherCollectionProps}
-						/>
-					)
-				)}
-			</div>
-		);
-	}
-}
+const maptStateToProps = createStructuredSelector({
+	collections : selectCollections,
+});
 
-export default ShopPage;
+export default connect(maptStateToProps)(ShopPage);
