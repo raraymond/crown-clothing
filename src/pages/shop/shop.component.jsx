623682d169292d30e6ddd056f78a8290f606629d
Ryan Raymond <ryan@iMac.local>
2020-04-23 08:09:20 -0400

    converted component did mount to a promise pattern rather than a live snapshop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -27,13 +27,11 @@ class ShopPage extends React.Component {
 		const { updateCollections } = this.props;
 		const collectionRef = firestore.collection('collections');
 
-		this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-			async (snapshot) => {
-				const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-				updateCollections(collectionsMap);
-				this.setState({ loading: false });
-			}
-		);
+		collectionRef.get().then((snapshot) => {
+			const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+			updateCollections(collectionsMap);
+			this.setState({ loading: false });
+		});
 	}
 
 	render () {
