f287a29a9ed70105cc9b7f4184457d3f805f899f
Ryan Raymond <ryan@iMac.local>
2020-04-25 11:30:24 -0400

    added hooks to sign in sign up and shop components

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,31 +7,30 @@ import collectionsOverViewContainer from '../../components/collections-overview/
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-class ShopPage extends React.Component {
-	componentDidMount () {
-		const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ match, fetchCollectionsStart }) => {
+	useEffect(
+		() => {
+			fetchCollectionsStart();
+		},
+		[
+			fetchCollectionsStart,
+		]
+	);
 
-		fetchCollectionsStart();
-	}
-
-	render () {
-		const { match } = this.props;
-
-		return (
-			<div className='shop-page'>
-				<Route
-					exact
-					path={`${match.path}`}
-					component={collectionsOverViewContainer}
-				/>
-				<Route
-					path={`${match.path}/:collectionId`}
-					component={collectionPageContainer}
-				/>
-			</div>
-		);
-	}
-}
+	return (
+		<div className='shop-page'>
+			<Route
+				exact
+				path={`${match.path}`}
+				component={collectionsOverViewContainer}
+			/>
+			<Route
+				path={`${match.path}/:collectionId`}
+				component={collectionPageContainer}
+			/>
+		</div>
+	);
+};
 
 const mapDispatchToProps = (dispatch) => ({
 	fetchCollectionsStart : () => dispatch(fetchCollectionsStart()),
